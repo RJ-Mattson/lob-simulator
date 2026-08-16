9521280d4b0988c7ececf29999921aa3c4a9b381
RJ Mattson <rjmatts@gmail.com>
2026-08-16 14:52:18 -0500

    Order book structure

---
+++ include/order_book.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "order.hpp"
+
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <vector>
+#include <optional>
+#include <functional>
+#include <cstdint>
+
+namespace lob {
+    struct PriceLevel{
+        std::deque<Order> 
+    }
+}
